db5b5110a03d50c60a24b8106214373b7d4d35fc
Danish <danish.belal100@gmail.com>
2012-09-25 14:36:17 +0200

    Added ggT and Prime_Math.java
    Have a nice Day !

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,6 +2,7 @@ package project;
 
 // import project.model.Prime;
 import project.model.Prime;
+import project.model.Prime_Math;
 
 public class Main
 {
@@ -13,12 +14,15 @@ public class Main
         // ui.initGUI();
 
         long before = System.currentTimeMillis();
-        int maxPrime = (int) Math.sqrt( Integer.MAX_VALUE ) - 1;
-        // Prime test=new Prime();
+//        int maxPrime = (int) Math.sqrt( Integer.MAX_VALUE ) - 1;
+        
 
-        Prime p = new Prime();
-        p.calcPrimes( maxPrime );
+//        Prime p = new Prime();
+//        p.calcPrimes( maxPrime );
 
+        System.out.println(Prime_Math.ggT( Prime_Math.EUKLID, 17, 34 ));
+        
+        
         System.out.println( "Took " + ( System.currentTimeMillis() - before )
                 + "ms" );
         System.out.println( "Used Memory: "
---
+++ src/project/model/Prime_Math.java
@@ -0,0 +1,54 @@
+package project.model;
+
+public class Prime_Math // TODO : Rename
+{
+
+    public final static int EUKLID = 0x1;
+    public final static int PRIME_ZERLEGUNG = 0x2; // TODO : Rename !
+
+    public static long kgV(int method, long first, long second)
+    {
+        switch (method)
+        {
+        case EUKLID:
+
+            break;
+
+        case PRIME_ZERLEGUNG:
+
+            break;
+
+        default:
+            throw new IllegalArgumentException( " the method is not defined ! " );
+        }
+
+        return 0L;
+    }
+
+    public static long ggT(int method, long first, long second)
+    {
+        switch (method)
+        {
+        case EUKLID:
+            return ggT_Euklid( first, second );
+
+        default:
+            throw new IllegalArgumentException( "method not defined !" );
+        }
+    }
+
+    private static long ggT_Euklid(long first, long second)
+    {
+        if ( first == 0 )
+            return second;
+
+        for ( ; second != 0; )
+            if ( first > second )
+                first -= second;
+            else
+                second -= first;
+
+        return first;
+
+    }
+}

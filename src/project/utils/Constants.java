bc4f5e36a03dcae318404118916351ef0a54e018
Danish <danish.belal100@gmail.com>
2012-09-30 18:07:13 +0200

    Some Changes...

---
+++ src/project/utils/Constants.java
@@ -0,0 +1,20 @@
+package project.utils;
+
+public class Constants
+{
+//    Numeric Constants :
+    
+    public final static int WINDOW_WIDTH = 500;
+    public final static int WINDOW_HEIGHT = 300;
+    
+    
+//    String Constants
+    public final static String APP_VERSION = " v0.1 Developer Preview";
+    public final static String DEVELOPER_NAMES = "Jan-Erik Petersen und Danish Belal";
+    public final static String WINDOW_TITLE = "Primes " + " von " + DEVELOPER_NAMES + APP_VERSION ;
+    
+    public final static String PRIME_TAB_NAME = "Primzahlen";
+    
+    
+    
+}
